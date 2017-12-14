b6f4adcbec3d4c517d356efc6303a1b71559761e
noahpulsipher <31518741+noahpulsipher@users.noreply.github.com>
2017-12-14 13:25:11 -0700

    Created a pokemon class in the model package, and a method to return the types for each pokemon.

---
+++ src/pokemon/model/Pokemon.java
@@ -0,0 +1,46 @@
+package pokemon.model;
+
+public abstract class Pokemon 
+{
+	private int healthPoints;
+	private int attackPoints;
+	private double enhancementMoifier;
+	private int number;
+	private String name;
+	private boolean canEvolve;
+	
+	public Pokemon (int number, String name)
+	{
+		this.name = name;
+		this.number = number;
+	}
+	
+	public final String[] getPokemonTypes()
+	{
+		Class<?> [] types = getClass().getInterfaces();
+		String [] pokeTypes = new String[types.length];
+		
+		for(int index = 0; index < types.length; index++)
+		{
+			String currentInterface = types[index].getCanonicalName();
+			currentInterface = currentInterface.replace(this.getClass().getPackage().getName() + ".", "");
+			pokeTypes[index] = currentInterface;
+		}
+		
+		return pokeTypes;
+	}
+	
+	public String toString()
+	{
+		String description = "Hi, I am a " + name + ", and my HP is " + healthPoints;
+		
+		return description;
+	}
+	
+	public String getPokemonInformation()
+	{
+		String pokemonInfo = "This pokemon is of type: " + this.getClass().getSimpleName();
+		
+		return pokemonInfo;
+	}
+}
